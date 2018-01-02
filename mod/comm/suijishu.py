4a571fbb32711e320fab88ae8236210f0f469fab
leididi <807782172@qq.com>
2018-01-02 21:44:36 +0800

    随机数模板

--- mod/comm/suijishu.py
+++ mod/comm/suijishu.py
@@ -36,9 +36,45 @@ def generate_verification_code2():
     print code_list
     verification_code = ''.join(code_list)
     return verification_code
-
+def get_phone_num1():
+    phone_num = ['187','132']
+    #手机号区域
+    random_num = choice(phone_num)
+    code_list = []
+    # for i in range(8):
+    #     code_list.append(str(random.randrange(0,9)))
+    for i in range(10):
+        code_list.append(str(i))
+    myslice =random.sample(code_list,8)
+    code = ''.join(myslice)
+    result = random_num+code
+    print result
+def get_phone_num2(phone_name):
+    #电信
+    phone_DX = ['133','153','177','180','181','189']
+    #联通
+    phone_LT = ['130','131','132','155','156','185','186','145','176']
+    #移动
+    phone_YD = ['134','135','136','137','138','139','147','150','151','152','157','158','159','178','182','183','184','187','188']
+    #手机号区域
+    if  phone_name == '电信':
+        random_num = choice(phone_DX)
+    elif phone_name == '联通':
+        random_num =choice(phone_LT)
+    elif phone_name == '移动':
+        random_num = choice(phone_YD)
+    else:
+        return  'input parameter'
+    code_list = []
+    for i in range(8):
+        code_list.append(str(random.randrange(0,9)))
+    code = ''.join(code_list)
+    result = random_num+code
+    print result
 if __name__ == '__main__':
-    code = generate_verification_code()
-    code2 = generate_verification_code2()
-    print code
-    print code2
+    # code = generate_verification_code()
+    # code2 = generate_verification_code2()
+    # print code
+    # print code2
+    get_phone_num1()
+    get_phone_num2('联通')
